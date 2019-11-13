bc93eceb1f70c8d01efdfb1a3d7c338ea5e119c0
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-11-13 07:39:25 -0500

    fixed comments for description of solution

--- general/pascals_triangle.rb
+++ general/pascals_triangle.rb
@@ -1,17 +1,23 @@
 #!/usr/bin/env ruby
 
+# O(n^2)
+# Binomial Coefficient
 def pascals_triangle(nth_iteration)
   pascal = []
 
   nth_iteration.times do |line|
-    c = 1
-    l = line + 1
+    c = 1 # first value is always 1
+    l = line + 1 # .times starts `line` at 0
 
     row = []
     l.times do |iteration|
-      i = iteration + 1
+      i = iteration + 1 # .times starts `iteration` at 0
 
+      # add calculated line value
       row << c
+
+      # recalculate next line value
+      # using binomial coefficient formula
       c = (c * (l - i) / i)
     end
 
